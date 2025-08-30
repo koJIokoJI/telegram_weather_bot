3b03f1aaae8087c2c7e5cca0409369aa897dbd94
mia <mxssedxnactxon@gmail.com>
2025-08-30 12:51:59 +0700

    dont know

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ import sys
 
 from config.config import settings
 from app.bot import bot
+from logs.telegram_log_handler import TgHandler
 
-logging.basicConfig(level=settings.logs.level, format=settings.logs.format)
+logger = logging.getLogger(__name__)
+tg_handler = TgHandler(bot_token=settings.log_bot_token, chat_id=settings.admin_ids[0])
+tg_handler.setLevel(logging.ERROR)
+logging.basicConfig(
+    level=settings.logs.level,
+    format=settings.logs.format,
+    handlers=[tg_handler, logging.StreamHandler()],
+)
 
 if sys.platform.startswith("win") or os.name == "nt":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
